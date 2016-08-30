6fb0a1286a8888b5bca59625a1804c32a5893f5b
thomp <thompickett@gmail.com>
2016-08-29 18:40:44 -0600

    progress

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 
 const path = require('path');
 
+
 app.use(express.static('static'));
 
 app.set('port', process.env.PORT || 3000);
@@ -12,8 +13,12 @@ app.get('/', (request,response) => {
   response.sendFile(path.join(__dirname, '/static/index.html'));
 });
 
+app.get('/gallery', (request,response) => {
+  response.sendFile(path.join(__dirname, '/static/gallery.html'));
+});
+
 app.get('/main-page-background.jpg', function (request,response) {
-  response.sendFile(path.join(__dirname, '/static/photos/spiral_stair_draft.jpg'));
+  response.sendFile(path.join(__dirname, '/static/photos/practice.png'));
 });
 
 app.get('/light-for-routes.png', function (request,response) {
@@ -30,6 +35,18 @@ app.get('/light-for-routes-alt.png', function (request,response) {
 
 app.get('/background.jpg', function (request,response) {
   response.sendFile(path.join(__dirname, '/static/photos/background.jpg'));
+});
+
+app.get('/portal.png', function (request,response) {
+  response.sendFile(path.join(__dirname, '/static/photos/portal.png'));
+});
+
+app.get('/portal-two.png', function (request,response) {
+  response.sendFile(path.join(__dirname, '/static/photos/portal-two.png'));
+});
+
+app.get('/grass-gate.jpg', function (request,response) {
+  response.sendFile(path.join(__dirname, '/static/photos/grass-gate.jpg'));
 });
 
 if (!module.parent) {
